efbbb7ac8faee12e2ca07d20a95f93a88534fd80
Zulfikar <61768347+vickycxc@users.noreply.github.com>
2020-10-18 08:24:55 +0800

    Add whatsapp instance

---
+++ app.js
@@ -0,0 +1,21 @@
+const { Client } = require('whatsapp-web.js');
+const client = new Client({puppeteer: {args: ['--no-sandbox']}});
+
+client.initialize();
+
+client.on('qr', (qr) => {
+    console.log('QR:', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    if (message.body === '!ping') {
+        client.sendMessage(message.from, 'pong');
+    }
+    if (message.body === '!pingg') {
+        client.sendMessage(message.from, message.from);
+    }
+});
