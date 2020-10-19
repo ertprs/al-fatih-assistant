748dc5fd34b2d271048665d4963a21d08cfcbfd7
Zulfikar <61768347+vickycxc@users.noreply.github.com>
2020-10-19 08:49:53 +0800

    Add session

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const { Client } = require('whatsapp-web.js');
 const client = new Client({puppeteer: {args: ['--no-sandbox']}});
+const fs = require('fs');
 
-client.initialize();
+const sessionData = JSON.parse(process.env.WA_KEY);
+
+client.initialize({
+    session: sessionData});
 
 client.on('qr', (qr) => {
     console.log('QR:', qr);
+});
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFile('./session.json', JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
 });
 
 client.on('ready', () => {
